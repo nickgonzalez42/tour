c45f3f89c8368c6f36e6d9be17ec72c9ef106eed
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-05-04 11:41:04 -0500

    button and UI update

--- src/App.jsx
+++ src/App.jsx
@@ -31,34 +31,22 @@ function App() {
   };
 
   return (
-    <div className="bg-slate-400 grid place-items-center w-screen">
+    <div className="grid place-items-center w-screen">
       <ReactPhotoSphereViewer ref={photoSphereRef} src={`/70${currentPhoto}.jpg`} height={"100vh"} width={"100vw"} />
       <div className="bg-black min-w-fit h-20 bg-opacity-40 absolute top-0 flex items-center justify-between">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="shrink-0 h-3/5 m-8 cursor-pointer bg-white border rounded-full border-black border-solid"
+        <button
+          className="text-white bg-gradient-to-br from-yellow-600 to-red-500 hover:bg-gradient-to-bl focus:ring-red-300 dark:focus:ring-yellow-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ml-2"
           onClick={handleDecrementPhoto}
-          type="button"
         >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-        </svg>
-        <p className="text-white font-serif lg:text-3xl shrink">McDonald&apos;s 1st Store Museum</p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="shrink-0 h-3/5 m-8 cursor-pointer bg-white border rounded-full border-black border-solid"
+          Move Back
+        </button>
+        <p className="text-white font-sans lg:text-3xl shrink">McDonald&apos;s 1st Store Museum</p>
+        <button
+          className="text-white bg-gradient-to-br from-red-600 to-yellow-500 hover:bg-gradient-to-bl focus:ring-yellow-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-2 py-2.5 text-center mr-2 mb-2 ml-2"
           onClick={handleIncrementPhoto}
-          type="button"
         >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-        </svg>
+          Move Forward
+        </button>
       </div>
     </div>
   );
